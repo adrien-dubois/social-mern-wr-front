4c14df83ce9393561f7e4ed6ddfed1e7ee4c577b
adrien-dubois <mcsnoos@gmail.com>
2022-06-09 23:34:04 +0200

    [DESIGN]Switch buttons on form

--- src/components/Log/Log.elements.tsx
+++ src/components/Log/Log.elements.tsx
@@ -4,29 +4,47 @@ export const Div = styled.div`
     .form-container{
         margin: 0 auto;
         width: 650px;
-        height: 800px;
+        height: 620px;
         display: grid;
-        grid-template-columns: 35% 65%;
-        
+        grid-template-columns: 30% 70%;
+
         &__row{
-            margin-top: 2rem;
-            display: flex;
-            justify-content: center;
-
-            &__buttons{
-                display: flex;
-                flex-direction: column;
-                width: 60%;
-                margin-left: .8rem;
-
-                .text-info{
-                    margin-top: 1rem;
-                    font-size: .9rem;
-                    font-style: italic;
-                    color: var(--opaque-bg);
-                }
-            }
+            border-radius:  16px 0 0 16px ;
+            background: rgba(255, 255, 255, 0.05);
+            backdrop-filter: blur(5px);
+            -webkit-backdrop-filter: blur(5px);
+            border-top: 1px solid rgba(255, 255, 255, 0.3);
+            border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+            border-left: 1px solid rgba(255, 255, 255, 0.3);
             
+
+            &::before{
+                content: "";
+                position: absolute;
+                height: 100%;
+                width: 4px;
+                top: 0;
+                bottom: 0;
+                right: 0;
+                background: linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(244,59,71,1) 35%, rgba(69,58,148,1) 100%);
+            }
+
+            .sidebar{
+                &__ul{
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    padding-left: 5px;
+                    padding-top: 40px;
+
+                    &__li{
+                        position: relative;
+                        width: 94.5%;
+                    }
+                }
+                
+            }
         }
 
         .sign-component{
@@ -34,6 +52,16 @@ export const Div = styled.div`
             justify-content: flex-start;
             align-items: center;
             flex-direction: column;
+            /* height: 85%; */
+
+             /*---- glassmorph ----*/
+            border-radius: 0 16px 16px 0;
+            background: rgba(255, 255, 255, 0.05);
+            backdrop-filter: blur(5px);
+            -webkit-backdrop-filter: blur(5px);
+            border-top: 1px solid rgba(255, 255, 255, 0.3);
+            border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+            border-right: 1px solid rgba(255, 255, 255, 0.3);
 
             .error-auth{
                 font-style: italic;
@@ -43,6 +71,7 @@ export const Div = styled.div`
             }
             
             &__title{
+                margin-top: 1rem;
                 display: flex;
                 flex-direction: column;
                 align-items: center;
@@ -60,27 +89,52 @@ export const Div = styled.div`
 `;
 
 export const Buttons = styled.button`
-    border-radius: 50px;
     margin-top: 1rem;
     font-family: var(--poppins-font);
     background-color: transparent;
-    padding: 6px 16px;
-    color: #fff;
+    padding: 10px 16px;
     font-size: 1rem;
     font-weight: 500;
-    outline: none;
-    border: none;
+    position: relative;
+    display: block;
+    width: 100%;
+    display: flex;
+    color: var(--white-color);
+    border-radius: 20px;
     cursor: pointer;
-    transition: all .3s ease-out;
-    
-    &:hover, &.active{
-        background-image: linear-gradient(to right, #f43b47 0%, #453a94 100%);
-        background-size: 200% auto;
-        white-space: nowrap;
+    transition: all .4s ease-out;
+
+    &:hover{
         box-shadow: 0 0 10px #eee;
     }
-    
-    @media screen and (max-width: 960px){
-        width: 100%;
+
+    &.active{
+        background: linear-gradient(to right, #f43b47 0%, #453a94 100%);
+        background-size: 200% auto;
+        transition: all .4s ease-out;
+        
+        &:hover{
+            background-position: right center;
+            box-shadow: 0 0 10px #eee;
+            
+        }
+    }
+
+    &__icon{
+        position: relative;
+        display: block;
+        min-width: 60px;
+        height: 60px;
+        line-height: 70px;
+        text-align: center;
+    }
+
+    &__text{
+        position: relative;
+        display: block;
+        padding-left: 10px;
+        height: 60px;
+        line-height: 55px;
+        white-space: nowrap;
     }
 `;
--- src/components/Log/Log.tsx
+++ src/components/Log/Log.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent, useState } from 'react'
+import { FunctionComponent,  useState } from 'react'
 import { IconContext } from 'react-icons';
 import { FaUserCircle, FaUserPlus } from 'react-icons/fa';
-import ErrorNotification from '../../utils/ErrorNotifications';
 import SigninForm from '../SigninForm/SigninForm';
 import SignupForm from '../SignupForm/SignupForm';
 import { Buttons, Div } from './Log.elements'
+import { IoMdLogIn } from 'react-icons/io';
+import { FaUserEdit } from 'react-icons/fa';
 
 type LogProps = {
   signin: boolean,
@@ -32,32 +33,41 @@ const Log: FunctionComponent<LogProps> = ({
   return (
     <Div>
         <div className="form-container">
+
+            {/* TAB PART */}
             <div className='form-container__row'>
-              <div className="form-container__row__buttons">
-                
-                <Buttons 
-                  id="login" 
-                  onClick={handleForm}
-                  className={signIn ? "active" : ""} 
-                >
-                  Connection
-                </Buttons>
+            <IconContext.Provider value={{ size: "1.8rem" }}>
+             
+                  <div className="sidebar">
+                      <ul className="sidebar__ul">
 
-                <Buttons 
-                  id="register" 
-                  onClick={handleForm} 
-                  className={signUp ? "active" : ""}
-                >
-                  Inscription
-                </Buttons>
+                          <li className="sidebar__ul__li">
+                              <Buttons 
+                                className={ signUp ? 'sidebar__ul__li__a active' : 'sidebar__ul__li__a'}   
+                                id='register'
+                                onClick={handleForm}
+                              >
+                                <FaUserEdit/>&nbsp; Inscription
+                              </Buttons>
+                          </li>
 
-                {signIn ? 
-                         (<p className='text-info'>Pas encore inscrit? Enregistrez-vous </p>):
-                         (<p className='text-info'>Déjà inscrit? Connectez vous</p>)  
-              }
-              </div>
+                          <li className="sidebar__ul__li">
+                              <Buttons 
+                                className={ signIn ? 'sidebar__ul__li__a active' : 'sidebar__ul__li__a'}  
+                                id='login'
+                                onClick={handleForm}
+                              >
+                                  <IoMdLogIn/>&nbsp; Connexion
+                              </Buttons>
+                          </li>
+
+                      </ul>
+                  </div>
+              
+          </IconContext.Provider>
             </div>
 
+            {/* FORM PART */}
             <div className="sign-component">
               <IconContext.Provider value={{ color: "#FFF", size: "4rem" }}>
                 <div className="sign-component__title">
@@ -69,6 +79,7 @@ const Log: FunctionComponent<LogProps> = ({
               { signIn && <SigninForm/> }
               { signUp && <SignupForm/> }
             </div>
+
         </div>
     </Div>
   )
